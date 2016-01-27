8c62e77e403f6b5334ed770cc53c3e2f8fe5ef35
Damien Bellenger <damien.bellenger@gmail.com>
2016-01-27 14:33:20 +0100

    Download

--- src/fr/ineo/ping16/DevisActivity.java
+++ src/fr/ineo/ping16/DevisActivity.java
@@ -1,101 +1,99 @@
 package fr.ineo.ping16;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.gson.Gson;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import dto.Affaire;
-import json.DownloadAction;
+import json.DownloadTask;
 import json.ListeDocumentRequete;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class DevisActivity extends Activity {
-	
+public class DevisActivity extends Activity implements OnClickListener{
+
 	private Spinner devisspinner;
 	private Button btnSubmit;
-	
+
+	private ProgressDialog mProgressDialog;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_devis);
-		
-		
-		
+
+
+
 		addListenerOnButton();
-		addListenerOnSpinnerItemSelection();
-		
-		
+		devisspinner = (Spinner) findViewById(R.id.devisspinner);
+
+
 		SharedPreferences sharedPreferences = getSharedPreferences("mesPrefs", MODE_PRIVATE);
 		Gson gson = new Gson();
 		String jsonAffaire = sharedPreferences.getString("affaire", "");
 		Affaire affaire = gson.fromJson(jsonAffaire, Affaire.class);
-       
+
 		new ListeDocumentRequete().execute(affaire, devisspinner, this.getApplicationContext());
 		Button downloadButton = (Button) findViewById(R.id.downloadButton);
-		downloadButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-
-				new DownloadAction().execute(devisspinner.getSelectedItem().toString(), getApplicationContext());
-			}
-		});
+		downloadButton.setOnClickListener(this);
 	}
-	// add items into spinner dynamically
-	 
-
-	  public void addListenerOnSpinnerItemSelection() {
-		devisspinner = (Spinner) findViewById(R.id.devisspinner);
-		devisspinner.setOnItemSelectedListener(new OnItemSelectedListener(){
-
-			@Override
-			public void onItemSelected(AdapterView<?> parent, View view,
-					int position, long id) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> parent) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
-	  }
 
 
-	  // get the selected dropdown list value
-	  public void addListenerOnButton() {
+
+
+
+	// get the selected dropdown list value
+	public void addListenerOnButton() {
 
 		devisspinner = (Spinner) findViewById(R.id.devisspinner);	
 		btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
 		btnSubmit.setOnClickListener(new OnClickListener() {
 
-		  @Override
-		 
-			  public void onClick(View v) {
-				  
-			 
-			  	Intent intent = new Intent(DevisActivity.this, ChoixDevisActivity.class);
+			@Override
+
+			public void onClick(View v) {
+
+
+				Intent intent = new Intent(DevisActivity.this, ChoixDevisActivity.class);
 				startActivity(intent);
-					   }
-		    
-		  
-		  
+			}
+
+
+
 		});
-}
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		// instantiate it within the onCreate method
+		mProgressDialog = new ProgressDialog(this);
+		mProgressDialog.setMessage("Please wait ...");
+		mProgressDialog.setIndeterminate(true);
+		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		mProgressDialog.setCancelable(true);
+		
+		SharedPreferences sharedPreferences = getSharedPreferences("mesPrefs", MODE_PRIVATE);
+		Gson gson = new Gson();
+		String jsonAffaire = sharedPreferences.getString("affaire", "");
+		Affaire affaire = gson.fromJson(jsonAffaire, Affaire.class);
+		
+		// execute this when the downloader must be fired
+		final DownloadTask downloadTask = new DownloadTask(this, mProgressDialog);
+		downloadTask.execute(affaire.getNom(), devisspinner.getSelectedItem().toString());
+
+		mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				downloadTask.cancel(true);
+			}
+		});
+	}
 }
---
+++ src/json/DownloadTask.java
@@ -0,0 +1,129 @@
+package json;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.PowerManager;
+import android.widget.Toast;
+
+public class DownloadTask extends AsyncTask<String, Integer, String> {
+
+    private Context context;
+    private PowerManager.WakeLock mWakeLock;
+    private ProgressDialog progressDialog;
+	public final static String DOCUMENT = "http://faridchouakria.free.fr/documents/";
+
+    public DownloadTask(Context context, ProgressDialog progressDialog) {
+        this.context = context;
+        this.progressDialog = progressDialog;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, getClass().getName());
+        mWakeLock.acquire();
+        progressDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... progress) {
+        super.onProgressUpdate(progress);
+        progressDialog.setIndeterminate(false);
+        progressDialog.setMax(100);
+        progressDialog.setProgress(progress[0]);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mWakeLock.release();
+        progressDialog.dismiss();
+        if (result != null)
+            Toast.makeText(context, "Download error: " + result, Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(context, "File downloaded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected String doInBackground(String... sUrl) {
+    	String affaireName = sUrl[0];
+    	String fileName = sUrl[1];
+    	
+    	File repPrincipal = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/GestIneo");
+    	repPrincipal.mkdirs();
+    	System.out.println(repPrincipal.getAbsolutePath());
+
+    	File repAffaire = new File(repPrincipal, affaireName);
+    	repAffaire.mkdirs();
+    	System.out.println(repAffaire.getAbsolutePath());
+
+    	File file = new File(repAffaire, fileName);
+    	System.out.println(file.getAbsolutePath());
+
+        InputStream input = null;
+        OutputStream output = null;
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(DOCUMENT + sUrl[0] + "/" + sUrl[1]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            // expect HTTP 200 OK, so we don't mistakenly save error report
+            // instead of the file
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                return "Server returned HTTP " + connection.getResponseCode()
+                        + " " + connection.getResponseMessage();
+            }
+
+            // this will be useful to display download percentage
+            // might be -1: server did not report the length
+            int fileLength = connection.getContentLength();
+
+            // download the file
+            input = connection.getInputStream();
+            output = new FileOutputStream(file);
+
+            byte data[] = new byte[4096];
+            long total = 0;
+            int count;
+            while ((count = input.read(data)) != -1) {
+                // allow canceling with back button
+                if (isCancelled()) {
+                    input.close();
+                    return null;
+                }
+                total += count;
+                // publishing the progress....
+                if (fileLength > 0) // only if total length is known
+                    publishProgress((int) (total * 100 / fileLength));
+                output.write(data, 0, count);
+            }
+        } catch (Exception e) {
+            return e.toString();
+        } finally {
+            try {
+                if (output != null)
+                    output.close();
+                if (input != null)
+                    input.close();
+            } catch (IOException ignored) {
+            }
+
+            if (connection != null)
+                connection.disconnect();
+        }
+        return null;
+    }
+
+
+}
