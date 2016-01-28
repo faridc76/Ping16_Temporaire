36e051d7896b084cf7f2ea14c15ffaf9d632e706
Damien Bellenger <damien.bellenger@gmail.com>
2016-01-28 02:29:06 +0100

    Upload terminé http://tools.ietf.org/html/rfc1867

--- src/fr/ineo/ping16/DevisActivity.java
+++ src/fr/ineo/ping16/DevisActivity.java
@@ -13,87 +13,116 @@ import android.view.View.OnClickListener;
 import dto.Affaire;
 import json.DownloadTask;
 import json.ListeDocumentRequete;
+import json.UploadTask;
 import android.widget.Button;
 import android.widget.Spinner;
 
 public class DevisActivity extends Activity implements OnClickListener{
 
-	private Spinner devisspinner;
-	private Button btnSubmit;
+    private Spinner devisspinner;
+    private Button btnSubmit;
+    private Button downloadButton;
+    private Button uploadButton;
 
-	private ProgressDialog mProgressDialog;
+    private ProgressDialog mProgressDialog;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_devis);
-
-
-
-		addListenerOnButton();
-		devisspinner = (Spinner) findViewById(R.id.devisspinner);
-
-
-		SharedPreferences sharedPreferences = getSharedPreferences("mesPrefs", MODE_PRIVATE);
-		Gson gson = new Gson();
-		String jsonAffaire = sharedPreferences.getString("affaire", "");
-		Affaire affaire = gson.fromJson(jsonAffaire, Affaire.class);
-
-		new ListeDocumentRequete().execute(affaire, devisspinner, this.getApplicationContext());
-		Button downloadButton = (Button) findViewById(R.id.downloadButton);
-		downloadButton.setOnClickListener(this);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devis);
 
 
 
+        addListenerOnButton();
+        devisspinner = (Spinner) findViewById(R.id.devisspinner);
 
 
-	// get the selected dropdown list value
-	public void addListenerOnButton() {
+        SharedPreferences sharedPreferences = getSharedPreferences("mesPrefs", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String jsonAffaire = sharedPreferences.getString("affaire", "");
+        Affaire affaire = gson.fromJson(jsonAffaire, Affaire.class);
 
-		devisspinner = (Spinner) findViewById(R.id.devisspinner);	
-		btnSubmit = (Button) findViewById(R.id.btnSubmit);
-
-		btnSubmit.setOnClickListener(new OnClickListener() {
-
-			@Override
-
-			public void onClick(View v) {
-
-
-				Intent intent = new Intent(DevisActivity.this, ChoixDevisActivity.class);
-				startActivity(intent);
-			}
+        new ListeDocumentRequete().execute(affaire, devisspinner, this.getApplicationContext());
+        downloadButton = (Button) findViewById(R.id.downloadButton);
+        downloadButton.setOnClickListener(this);
+        uploadButton = (Button) findViewById(R.id.uploadButton);
+        uploadButton.setOnClickListener(this);
+    }
 
 
 
-		});
-	}
 
-	@Override
-	public void onClick(View v) {
 
-		// instantiate it within the onCreate method
-		mProgressDialog = new ProgressDialog(this);
-		mProgressDialog.setMessage("Please wait ...");
-		mProgressDialog.setIndeterminate(true);
-		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-		mProgressDialog.setCancelable(true);
-		
-		SharedPreferences sharedPreferences = getSharedPreferences("mesPrefs", MODE_PRIVATE);
-		Gson gson = new Gson();
-		String jsonAffaire = sharedPreferences.getString("affaire", "");
-		Affaire affaire = gson.fromJson(jsonAffaire, Affaire.class);
-		
-		// execute this when the downloader must be fired
-		final DownloadTask downloadTask = new DownloadTask(this, mProgressDialog);
-		downloadTask.execute(affaire.getNom(), devisspinner.getSelectedItem().toString());
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
 
-		mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-			@Override
-			public void onCancel(DialogInterface dialog) {
-				downloadTask.cancel(true);
-			}
-		});
-	}
+        devisspinner = (Spinner) findViewById(R.id.devisspinner);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(DevisActivity.this, ChoixDevisActivity.class);
+                startActivity(intent);
+            }
+
+
+
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == downloadButton) {
+            // instantiate it within the onCreate method
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Downloading ...");
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            mProgressDialog.setCancelable(true);
+
+            SharedPreferences sharedPreferences = getSharedPreferences("mesPrefs", MODE_PRIVATE);
+            Gson gson = new Gson();
+            String jsonAffaire = sharedPreferences.getString("affaire", "");
+            Affaire affaire = gson.fromJson(jsonAffaire, Affaire.class);
+
+            // execute this when the downloader must be fired
+            final DownloadTask downloadTask = new DownloadTask(this, mProgressDialog);
+            downloadTask.execute(affaire.getNom(), devisspinner.getSelectedItem().toString());
+
+            mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    downloadTask.cancel(true);
+                }
+            });
+        } else if(v == uploadButton) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Uploading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            mProgressDialog.setProgress(0);
+            mProgressDialog.show();
+
+            SharedPreferences sharedPreferences = getSharedPreferences("mesPrefs", MODE_PRIVATE);
+            Gson gson = new Gson();
+            String jsonAffaire = sharedPreferences.getString("affaire", "");
+            Affaire affaire = gson.fromJson(jsonAffaire, Affaire.class);
+
+            // execute this when the downloader must be fired
+            final UploadTask uploadTask = new UploadTask(this, mProgressDialog);
+            uploadTask.execute(affaire.getNom(), devisspinner.getSelectedItem().toString());
+
+            mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    uploadTask.cancel(true);
+                }
+            });
+        }
+    }
 }
---
+++ src/json/UploadTask.java
@@ -0,0 +1,147 @@
+package json;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.PowerManager;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UploadTask extends AsyncTask<String, Integer, String> {
+
+    private Context context;
+    private PowerManager.WakeLock mWakeLock;
+    private ProgressDialog progressDialog;
+
+    public UploadTask(Context context, ProgressDialog progressDialog) {
+        this.context = context;
+        this.progressDialog = progressDialog;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, getClass().getName());
+        mWakeLock.acquire();
+        progressDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... progress) {
+        super.onProgressUpdate(progress);
+        progressDialog.setIndeterminate(false);
+        progressDialog.setMax(100);
+        progressDialog.setProgress(progress[0]);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mWakeLock.release();
+        progressDialog.dismiss();
+        if(result.equals("true"))
+            Toast.makeText(context, "Upload terminé", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(context, "Echec de l'upload", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected String doInBackground(String... sUrl) {
+        String affaireName = sUrl[0];
+        String fileName = sUrl[1];
+
+        String boundary = "boundaryFile";
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+
+        String result = "";
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/GestIneo/" + affaireName, fileName);
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] bytes = new byte[(int) file.length()];
+            fileInputStream.read(bytes);
+            fileInputStream.close();
+
+            URL url = new URL("http://faridchouakria.free.fr/webservices/upload_fichier.php");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+            OutputStream outputStream = connection.getOutputStream();
+
+            DataOutputStream dos = new DataOutputStream(outputStream);
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"affaire\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(affaireName + lineEnd);
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"fichier\"; filename=\"" + fileName + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            int bufferLength = 1024;
+            for (int i = 0; i < bytes.length; i += bufferLength) {
+                int progress = (int)((i / (float) bytes.length) * 100);
+                publishProgress(progress);
+                if (bytes.length - i >= bufferLength) {
+                    outputStream.write(bytes, i, bufferLength);
+                } else {
+                    outputStream.write(bytes, i, bytes.length - i);
+                }
+            }
+            publishProgress(100);
+
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            dos.flush();
+            dos.close();
+
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String ligne;
+            while ((ligne = reader.readLine()) != null) {
+                System.out.println("ligne " + ligne);
+                result += ligne;
+            }
+
+            JSONObject obj = new JSONObject(result);
+            result = obj.getString("result");
+            inputStream.close();
+            connection.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return e.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+        return result;
+    }
+
+
+}
